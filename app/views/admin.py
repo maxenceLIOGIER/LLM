07a3181973b3c3882aff3696767b192cd972be99
berangerthomas <beranger.t@proton.me>
2025-01-29 20:18:41 +0100

    Simplification page admin

--- app/views/admin.py
+++ app/views/admin.py
@@ -10,9 +10,9 @@ def adm_page():
     st.title("Admin")
     st.write("Tableau des logs")
 
-    conn = sqlite3.connect(db_path)
-    total_data = pd.read_sql_query("SELECT * FROM log", conn)
-    conn.close()
+    # Lecture des données
+    with sqlite3.connect(db_path) as conn:
+        total_data = pd.read_sql_query("SELECT * FROM log", conn)
 
     if total_data.empty:
         total_data = pd.DataFrame(
@@ -20,33 +20,29 @@ def adm_page():
         )
 
     st.sidebar.header("Filtres")
-    items_per_page = st.sidebar.selectbox(
-        "Éléments par page", options=[10, 20, 50, 100], index=0
-    )
+    items_per_page = st.sidebar.selectbox("Éléments par page", [10, 20, 50, 100], 0)
 
-    total_filtered_items = len(total_data)
-    total_pages = max(1, (total_filtered_items - 1) // items_per_page + 1)
+    total_items = len(total_data)
+    total_pages = max(1, (total_items - 1) // items_per_page + 1)
 
     if "current_page" not in st.session_state:
         st.session_state.current_page = 1
 
+    # Sélection de la page
     current_page = st.sidebar.number_input(
         "Numéro de page",
-        min_value=1,
-        max_value=total_pages,
-        value=st.session_state.current_page,
-        step=1,
+        1,
+        total_pages,
+        st.session_state.current_page,
+        1,
         key="current_page",
     )
 
-    start_idx = (st.session_state.current_page - 1) * items_per_page
-    end_idx = min(start_idx + items_per_page, total_filtered_items)
-    st.write(
-        f"Affichage des éléments {start_idx + 1} à {end_idx} sur {total_filtered_items}"
-    )
+    # Pagination
+    start_idx = (current_page - 1) * items_per_page
+    end_idx = min(start_idx + items_per_page, total_items)
+    st.write(f"Affichage des éléments {start_idx + 1} à {end_idx} sur {total_items}")
 
     st.dataframe(
         total_data.iloc[start_idx:end_idx], use_container_width=True, hide_index=True
     )
-
-    st.metric("Nombre total d'éléments", total_filtered_items)
