15bb4d7e5b9b1e0d47966020a93cb4cff9a5cf19
berangerthomas <beranger.t@proton.me>
2025-01-30 08:48:09 +0100

    Ajout de la visibilité de toutes les tables dans Admin
    Avec un menu pour sélectionner la table à afficher

--- app/views/admin.py
+++ app/views/admin.py
@@ -7,18 +7,38 @@ db_path = Path(__file__).parent.parent.parent / "database" / "db_logs.db"
 
 
 def adm_page():
-    st.title("Admin")
-    st.write("Tableau des logs")
 
-    # Lecture des données
+    # Titre de la page
+    st.title("Admin")
+
+    # Connexion à la base de données
     with sqlite3.connect(db_path) as conn:
-        total_data = pd.read_sql_query("SELECT * FROM log", conn)
+        # Récupération des tables et des colonnes
+        tables = pd.read_sql_query(
+            "SELECT name FROM sqlite_master WHERE type='table'", conn
+        )
+        table_cols = {}
+        for t in tables["name"]:
+            cols = pd.read_sql_query(f"PRAGMA table_info({t})", conn)["name"].tolist()
+            table_cols[t] = cols
+
+    # Sélection de la table
+    options = list(table_cols.keys())
+    # Par défaut, on affiche la table "log"
+    default_table = "log"
+    default_index = options.index(default_table) if default_table in options else 0
+    selected_table = st.selectbox(
+        "Sélectionnez une table", options, index=default_index
+    )
+
+    # Récupération des données de la table sélectionnée
+    with sqlite3.connect(db_path) as conn:
+        total_data = pd.read_sql_query(f"SELECT * FROM {selected_table}", conn)
 
     if total_data.empty:
-        total_data = pd.DataFrame(
-            columns=["id_log", "timestamp", "id_prompt", "id_status", "id_origin"]
-        )
+        total_data = pd.DataFrame(columns=table_cols[selected_table])
 
+    # Filtrage des données
     st.sidebar.header("Filtres")
     items_per_page = st.sidebar.selectbox("Éléments par page", [10, 20, 50, 100], 0)
 
@@ -43,6 +63,7 @@ def adm_page():
     end_idx = min(start_idx + items_per_page, total_items)
     st.write(f"Affichage des éléments {start_idx + 1} à {end_idx} sur {total_items}")
 
+    # Affichage du tableau
     st.dataframe(
         total_data.iloc[start_idx:end_idx], use_container_width=True, hide_index=True
     )
