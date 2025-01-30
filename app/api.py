e04ba083afc520125d173803524a074c7e0ad2ad
berangerthomas <beranger.t@proton.me>
2025-01-30 20:00:35 +0100

    Ajout de l'API

---
+++ app/api.py
@@ -0,0 +1,82 @@
+from fastapi import FastAPI, Query
+from fastapi.responses import RedirectResponse
+import sqlite3
+from datetime import date
+from typing import Optional
+import os
+from pathlib import Path
+
+# Chemin de la base de données
+current_dir = Path(__file__).parent
+project_root = current_dir.parent
+dbpath = os.path.join(project_root, "database", "db_logs.db")
+
+# Instanciation de l'API FastAPI
+api = FastAPI(
+    title="SmartRescue API",
+    description="API for accessing SmartRescue logs",
+    version="1.0.0",
+)
+
+
+@api.get("/", include_in_schema=False)
+async def root():
+    # On redirige par défaut vers la doc
+    return RedirectResponse(url="/docs")
+
+
+# Requête de base, si l'utilisateur de l'API
+# ne spécifie pas de date de début ou de fin
+base_query = """
+    SELECT log.id_log, log.timestamp, prompt.prompt, prompt.response,
+    status.status, origin.response AS origin
+    FROM log
+    LEFT JOIN prompt ON log.id_prompt = prompt.id_prompt
+    LEFT JOIN status ON log.id_status = status.id_status
+    LEFT JOIN origin ON log.id_origin = origin.id_origin
+"""
+
+
+@api.get("/data")
+async def get_data(
+    start_date: Optional[date] = Query(None, description="Date de début (YYYY-MM-DD)"),
+    end_date: Optional[date] = Query(None, description="Date de fin (YYYY-MM-DD)"),
+):
+    try:
+        conn = sqlite3.connect(dbpath)
+        cursor = conn.cursor()
+
+        query = base_query
+        params = []
+
+        # Ajout des conditions de date si spécifiées
+        if start_date or end_date:
+            query += " WHERE "
+            conditions = []
+
+            if start_date:
+                conditions.append("date(log.timestamp) >= ?")
+                params.append(start_date.isoformat())
+
+            if end_date:
+                conditions.append("date(log.timestamp) <= ?")
+                params.append(end_date.isoformat())
+
+            query += " AND ".join(conditions)
+
+        query += " ORDER BY log.timestamp DESC"
+
+        cursor.execute(query, params)
+        rows = cursor.fetchall()
+
+        # Récupération des enregistrements
+        columns = [description[0] for description in cursor.description]
+        result = [dict(zip(columns, row)) for row in rows]
+
+        return result
+    except sqlite3.OperationalError as e:
+        return {"error": f"Database error: {str(e)}", "path": dbpath}
+    finally:
+        # pour cloturer proprement la connexion à la base de données
+        if "conn" in locals():
+            conn.close()
--- app/app.py
+++ app/app.py
@@ -1,16 +1,32 @@
-import os
 import base64
 import streamlit as st
 from streamlit_option_menu import option_menu
+import uvicorn
+from api import api
+from multiprocessing import Process
 from views.home import home_page
 from views.dashboard import dashboard_page
 from views.llm import llm_page
 from views.admin import adm_page
 
 APP_TITLE = "SmartRescue"
+API_PORT = 8901
+
+st.set_page_config(
+    page_title=APP_TITLE,
+    layout="wide",
+    initial_sidebar_state="expanded",
+    page_icon="assets/icone.png",
+)
 
 
 def add_logo():
+    """
+    Fonction d'insertion du logo en arrière-plan du menu latéral de Streamlit.
+
+    Il s'agit de la méthode conseillée par les mainteneurs de Streamlit :
+    https://discuss.streamlit.io/t/put-logo-and-title-above-on-top-of-page-navigation-in-sidebar-of-multipage-app/28213/6
+    """
     # Lecture du fichier image local
     with open("assets/logo.png", "rb") as f:
         logo_data = base64.b64encode(f.read()).decode()
@@ -31,29 +47,52 @@ def add_logo():
     )
 
 
-st.set_page_config(
-    page_title=APP_TITLE,
-    layout="wide",
-    initial_sidebar_state="expanded",
-    page_icon="assets/icone.png",
-)
+def run_fastapi(port: int) -> None:
+    """
+    Démarre le serveur API FastAPI en arrière-plan.
+
+    Le serveur est configuré pour écouter sur 127.0.0.1,
+    et le port spécifié en argument.
+
+    Param :
+            - port: Port d'écoute du serveur API
+    """
+    try:
+        uvicorn.run(api, host="127.0.0.1", port=port)
+    except Exception as e:
+        st.error(f"Échec du démarrage du serveur API : {str(e)}")
 
 
-with st.sidebar:
-    print(os.getcwd())
-    add_logo()
-    selected = option_menu(
-        menu_title="Navigation",
-        options=["Home", "LLM", "Dashboard", "Admin"],
-        icons=["house", "robot", "bar-chart", "shield"],
-        default_index=0,
-    )
+def main():
+    """
+    Fonction main de l'application Streamlit.
 
-if selected == "Home":
-    home_page()
-elif selected == "LLM":
-    llm_page()
-elif selected == "Dashboard":
-    dashboard_page()
-elif selected == "Admin":
-    adm_page()
+    On lance d'abord le serveur FastAPI en arrière-plan,
+    puis on affiche l'interface utilisateur Streamlit.
+    """
+    # Démarrer FastAPI en arrière-plan, dans un autre process
+    api_process = Process(target=run_fastapi, args=(API_PORT,), daemon=True)
+    api_process.start()
+
+    # Interface utilisateur Streamlit
+    with st.sidebar:
+        add_logo()
+        selected = option_menu(
+            menu_title="Navigation",
+            options=["Home", "LLM", "Dashboard", "Admin"],
+            icons=["house", "robot", "bar-chart", "shield"],
+            default_index=0,
+        )
+
+    if selected == "Home":
+        home_page()
+    elif selected == "LLM":
+        llm_page()
+    elif selected == "Dashboard":
+        dashboard_page()
+    elif selected == "Admin":
+        adm_page()
+
+
+if __name__ == "__main__":
+    main()
